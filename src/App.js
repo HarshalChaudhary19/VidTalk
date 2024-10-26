2350fda2cf66d5fbfc37173520ef095690b47e18
Harshal <harshalchaudhary03@gmail.com>
2024-10-26 10:08:21 +0530

    Added Frontend

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import  {typography,AppBar, Typography} from '@material-ui/core';//just like bootstrape
+import Videoplayer from './components/videoplayer';
+import Options from './components/options';
+import Notifications from './components/notifications';
+import {makeStyles} from '@material-ui/core/styles';
 
-function App() {
+const useStyles=makeStyles((theme)=>({//all the styles
+  appBar: {
+    borderRadius: 15,
+    margin: '30px 100px',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '600px',
+    border: '2px solid black',
+
+    [theme.breakpoints.down('xs')]: {
+      width: '90%',
+    },
+  },
+  image: {
+    marginLeft: '15px',
+  },
+  wrapper: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    width: '100%',
+  },
+}));
+
+const App = () => {
+  const classes=useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.wrapper}>
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography variant="h2" align='center'>VidTalk</Typography>
+      </AppBar>
+      <Videoplayer/>
+      <Options>
+        <Notifications/>{/* options mein ja kr child component se notifications import kro */}
+      </Options>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
